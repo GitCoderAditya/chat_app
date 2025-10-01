ddf835c2a8505dc8cea9309715123c883f276b8c
ashu <majumderaditya03@gmail.com>
2025-10-01 23:49:30 +0530

    message send from one client to other

--- fe/src/index.ts
+++ fe/src/index.ts
@@ -1,11 +1,27 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
 
 var user = 0;
 
+var allSockets: WebSocket[] = [];
+
 wss.on("connection", (ws) => {
+
+   allSockets.push(ws);
    user = user + 1;
 
    console.log(`user ${user} connected`)
+
+   ws.on("message", (message) => {
+      console.log(`received: ${message}`);
+
+      for(let i = 0; i < allSockets.length; i++){
+         const s = allSockets[i];
+         if (s) {
+            s.send(`${message} : sent from server `)
+         }
+      }
+   })
+
 });
